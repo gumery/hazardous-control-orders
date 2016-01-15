767c41b1f50338f71c9d0f45379ae8f0f58fc972
pihizi <pihizi@msn.com>
2016-01-15 04:10:14 +0000

    gini doctor

---
+++ class/Gini/Moudle/HazardousControlOrders.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Gini\Module;
+
+class HazardousControlOrders
+{
+    public static function setup()
+    {
+    }
+
+    public static function diagnose()
+    {
+        $errors = [];
+        $errors[] = "请确保cron.yml配置了定时刷新的命令\n\t\thazardous-control-orders:\n\t\t\tinterval: */5 * * * *\n\t\t\tcommand: hazardouscontrol relationshipop build\n\t\t\tcomment: 定时将订单的信息刷新到表里，为管理方查数据提供支持";
+        return $errors;
+    }
+}
+
+
