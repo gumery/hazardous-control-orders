dbd544c8d9789d5897a2829856a549fdbbdb97de
pihizi <pihizi@msn.com>
2016-01-27 02:59:50 +0000

    .

--- class/Gini/Module/HazardousControlOrders.php
+++ class/Gini/Module/HazardousControlOrders.php
@@ -11,7 +11,7 @@ class HazardousControlOrders
     public static function diagnose()
     {
         $errors = [];
-        $errors[] = "请确保cron.yml配置了定时刷新的命令\n\t\thazardous-control-orders:\n\t\t\tinterval: */5 * * * *\n\t\t\tcommand: hazardouscontrol relationshipop build\n\t\t\tcomment: 定时将订单的信息刷新到表里，为管理方查数据提供支持";
+        $errors[] = "请确保cron.yml配置了定时刷新的命令\n\t\thazardous-control-orders:\n\t\t\tinterval: '*/5 * * * *'\n\t\t\tcommand: hazardouscontrol relationshipop build\n\t\t\tcomment: 定时将订单的信息刷新到表里，为管理方查数据提供支持";
         return $errors;
     }
 }
