efdb2d890d78343652328e1d929b5b6459b60487
Jinlin Li <jinlin.li@geneegroup.com>
2016-05-13 19:19:11 +0800

    fix bug

--- class/Gini/Module/HazardousControlOrders.php
+++ class/Gini/Module/HazardousControlOrders.php
@@ -24,22 +24,27 @@ class HazardousControlOrders
     public static function getUnableProducts($e, $products, $group, $user)
     {
         if (!count($products)) return [];
-        $mode = \Gini\Config::get('hazardous.mode');
-        $data = [];
-        $conf = \Gini\Config::get('mall.rpc');
-        $client = \Gini\Config::get('mall.client');
-        $url  = $conf['lab-inventory']['url'];
-        $rpc  = \Gini\IoC::construct('\Gini\RPC', $url);
-        $token = $rpc->mall->authorize($client['id'], $client['secret']);
+        $mode    = \Gini\Config::get('hazardous.mode');
+        $conf    = \Gini\Config::get('mall.rpc');
+        $client  = \Gini\Config::get('mall.client');
+        $url     = $conf['lab-inventory']['url'];
+        $rpc     = \Gini\IoC::construct('\Gini\RPC', $url);
+        $group_id = $group->id;
+        $token   = $rpc->mall->authorize($client['id'], $client['secret']);
         if (!$token) return ['error' => H(T('存货管理连接中断'))];
         foreach ($products as $info) {
             if ($info['customized']) continue;
             $product = a('product', $info['id']);
             if (!$product->cas_no) continue;
-            $cas_no = $product->cas_no;
+            $cas_no  = $product->cas_no;
             $package = $product->package;
-            $i = \Gini\Unit\Conversion::of($product);
-            $volume = \Gini\ORM\Hazardous\Ulimit::getVolume($i, $cas_no);
+            $i       = \Gini\Unit\Conversion::of($product);
+            $ldata   = self::_getCasVolume($i, $cas_no, $group_id);
+            if ($ldata['error']) {
+                return $ldata['error'];
+            }
+            $volume = $ldata['volume'];
+
             if ((string)$volume === '') continue;
             // 如果设置为0不允许购买
             if ((string)$volume === '0') {
@@ -50,31 +55,29 @@ class HazardousControlOrders
                 ];
             }
             elseif ($volume) {
-                if (!$i->validate($volume)) {
-                    return ['error' => H(T('存量上限单位异常'))];
+                $pdata = self::_getProductVolume($i, $package, $info['quantity']);
+                if ($pdata['error']) {
+                    $data[] = [
+                        'reason' => $pdata['error'],
+                        'id' => $info['id'],
+                        'name' => $product->name
+                    ];
+                    continue;
+                }
+                $idata = self::_getInventoryVolume($cas_no, $group_id);
+                if ($idata['error']) {
+                    $data[] = [
+                        'reason' => $idata['error'],
+                        'id' => $info['id'],
+                        'name' => $product->name
+                    ];
+                    continue;
                 }
 
-                $pattern = '/^(\d+(?:\.\d+)?)([A-Za-zμ3]+)(\s?\\*\s?\d+)?$/i';
-                if (!preg_match($pattern, $package, $matches)) {
-                    return ['error' => H(T('商品包装异常'))];
-                }
-
-                // 商品包装 商品单位
-                $p_num = $matches[1];
-                $p_unit = $matches[2];
-                $pattern = '/^(\s?\\*\s?)(\d+)$/i';
-                if ( ($addition = $matches[3]) &&  preg_match($pattern, $addition, $mat)) {
-                    $mult = $mat[2];
-                    $p_num = $p_num * (int)$mult;
-                }
-
-                $pTotal = ($p_num * (int)$info['quantity']).$p_unit;
-
-                $total = $rpc->mall->inventory->getHazardousTotal($cas_no, $group->id);
                 // 存量超出上限
                 if ($mode == 'inv-limit') {
-                    $iNum = $i->from($total)->to('g');
-                    $pNum = $i->from($pTotal)->to('g');
+                    $iNum = $i->from($idata['volume'])->to('g');
+                    $pNum = $i->from($pdata['volume'])->to('g');
                     $lNum = $i->from($volume)->to('g');
                     $sum = $iNum +$pNum;
                     if ($sum > $lNum) {
@@ -87,7 +90,7 @@ class HazardousControlOrders
                 }
                 // 存量有无限制
                 elseif ($mode == 'inv-exists') {
-                    if (!$total) continue;
+                    if (!$iTotal) continue;
                     // 如果有存货就不可以购买
                     $data[] = [
                         'reason' => H(T('该商品有存货, 不允许购买')),
@@ -97,7 +100,85 @@ class HazardousControlOrders
                 }
             }
         }
-        return $data;
+        return $data?:[];
+    }
+
+
+
+    protected static $_RPCs = [];
+    protected static function getRPC($type = 'lab-inventory')
+    {
+        $confs = \Gini\Config::get('mall.rpc');
+        $conf = $confs[$type] ?: [];
+        if (!self::$_RPCs[$type]) {
+            $rpc = \Gini\IoC::construct('\Gini\RPC', $conf['url']);
+            self::$_RPCs[$type] = $rpc;
+            $client = \Gini\Config::get('mall.client');
+            if ($type == 'lab-inventory') {
+                $token = $rpc->mall->authorize($client['id'], $client['secret']);
+                if (!$token) {
+                    \Gini\Logger::of('lab-orders')
+                        ->error('Mall\\RObject getRPC: authorization failed with {client_id}/{client_secret} !',
+                            [ 'client_id' => $client['id'], 'client_secret' => $client['secret']]);
+                }
+            }
+        }
+
+        return self::$_RPCs[$type];
+    }
+
+    private static function _getCasVolume($i, $cas_no, $group_id) {
+        if (!$group_id){
+            return ['error'=>'组信息丢失'];
+        }
+        $cache = \Gini\Cache::of('cas-volume');
+        $key = "cas-volume[$cas_no][$group_id]";
+        $volume = $cache->get($key);
+        if (false === $volume) {
+            $rpc = self::getRPC('hazardous-control');
+            $criteria = ['cas_no'=>$cas_no, 'group_id'=>$group_id];
+            $volume = $rpc->inventory->getLimitVolume($criteria);
+            $cache->set($key, $volume, 86400);
+        }
+        if (!$i->validate($volume)) {
+            return ['error' => H(T('存量上限单位异常'))];
+        }
+        return ['volume'=>$volume];
+    }
+
+    private static function _getInventoryVolume($cas_no, $group_id) {
+        if (!$group_id){
+            return ['error'=>'组信息丢失'];
+        }
+        $cache = \Gini\Cache::of('cas-volume');
+        $key = "cas-volume[$cas_no][$group_id]";
+        $volume = $cache->get($key);
+        if (false === $volume) {
+            $rpc = self::getRPC('lab-inventory');
+            $criteria = ['cas_no'=>$cas_no, 'group_id'=>$group_id];
+            $volume = $rpc->inventory->getLimitVolume($criteria);
+            $cache->set($key, $volume, 15);
+        }
+        return ['volume' => $volume];
+    }
+
+    private static function _getProductVolume($i, $package, $quantity) {
+        $pattern = '/^(\d+(?:\.\d+)?)([A-Za-zμ3]+)(\s?\\*\s?\d+)?$/i';
+        if (!preg_match($pattern, $package, $matches)) {
+            return ['error' => H(T('商品包装异常'))];
+        }
+        // 商品包装 商品单位
+        $p_num = $matches[1];
+        $p_unit = $matches[2];
+        $pattern = '/^(\s?\\*\s?)(\d+)$/i';
+        if ( ($addition = $matches[3]) &&  preg_match($pattern, $addition, $mat)) {
+            $mult = $mat[2];
+            $p_num = $p_num * (int)$mult;
+        }
+
+        return [
+            'volume' => ($p_num * (int)$quantity).$p_unit,
+        ];
     }
 }
 
