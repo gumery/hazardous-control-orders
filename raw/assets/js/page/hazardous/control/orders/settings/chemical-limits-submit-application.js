cfa43395db06e6b5c8020191511c8a5d46b2d99d
pihizi <pihizi@msn.com>
2016-06-24 05:16:24 +0000

    。。

--- raw/assets/js/page/hazardous/control/orders/settings/chemical-limits-submit-application.js
+++ raw/assets/js/page/hazardous/control/orders/settings/chemical-limits-submit-application.js
@@ -1,8 +1,8 @@
 define('page/hazardous/control/orders/settings/chemical-limits-submit-application', ['jquery', 'bootbox'], function($, Bootbox) {
     $(document).on('submit', '.form-setting-requset-application-volume', function() {
-        var $that = $(this);
-        var url = $that.attr('action');
-        $.post(url, [], function(data) {
+        var $form = $(this);
+        var url = $form.attr('action');
+        $.post(url, $form.serialize(), function(data) {
             data = data || {};
             if (!data.code) {
                 window.location.reload();
