0b83d329a6e80830611c0141b44f5fc891401a37
洪小兵 <pihizi@msn.com>
2018-01-18 05:35:36 +0000

    存量上限申请需要增加普通化学品的支持，我们打算扩展chem-db的server，增加普通化学品。在这之前，我们先确保所有应用，没有对chem-db的产品结果强依赖：只要在chem-db查到数据，就标示是危险化学品

--- class/Gini/Module/HazardousControlOrders.php
+++ class/Gini/Module/HazardousControlOrders.php
@@ -197,12 +197,14 @@ class HazardousControlOrders
 
         $types = [
             'all' => '全部化学品',
+            'normal' => '普通化学品',
             'hazardous' => '危化品',
             'drug_precursor' => '易制毒',
             'explosive' => '易制爆',
             'highly_toxic' => '剧毒品',
             'psychotropic'=> '精神药品',
             'narcotic'=> '麻醉药品',
+            'gas'=> '气体',
         ];
 
         return $types;
